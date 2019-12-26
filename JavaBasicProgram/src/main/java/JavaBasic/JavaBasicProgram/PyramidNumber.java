54241ee8c6d40f269cb02e809f171e5e00feff3b
M1050915 <M1050915@A2ML31864.mindtree.com>
2019-12-26 14:37:08 +0530

    jenkins example pushed

---
+++ JavaBasicProgram/src/main/java/JavaBasic/JavaBasicProgram/PyramidNumber.java
@@ -0,0 +1,15 @@
+package JavaBasic.JavaBasicProgram;
+
+import java.util.Scanner;
+
+public class PyramidNumber {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		//System.out.println("Enter the size");
+		String s1="aaaa";
+		String s2="aaaz";
+		System.out.println(s1.compareTo(s2));
+				
+	}
+
+}
